6ad9285f686c7ce3ee2b22effa583d767c4bdc7b
zengweishi <805212031@qq.com>
2020-05-27 17:24:46 +0800

    Feign导致的启动失败问题配置

--- changgou-service/changgou-service-canal/src/main/java/com/changgou/canal/CanalApplication.java
+++ changgou-service/changgou-service-canal/src/main/java/com/changgou/canal/CanalApplication.java
@@ -14,7 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = {"com.changgou.content.feign"})
 @EnableCanalClient
 public class CanalApplication {
     public static void main(String[] args) {
