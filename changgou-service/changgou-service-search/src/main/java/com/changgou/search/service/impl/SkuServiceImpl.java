0ecc7250066c042ab04611f6331b6a52bfc06aa3
zengweishi <805212031@qq.com>
2020-05-29 14:46:57 +0800

    ES搜索条件增加品牌和分类

--- changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
+++ changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
@@ -7,6 +7,7 @@ import com.changgou.goods.pojo.Sku;
 import com.changgou.search.dao.SkuEsMapper;
 import com.changgou.search.pojo.SkuInfo;
 import com.changgou.search.service.SkuService;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.Aggregation;
@@ -61,7 +62,7 @@ public class SkuServiceImpl implements SkuService {
     @Override
     public Map search(Map<String, String> searchMap) {
         //条件构造
-        NativeSearchQueryBuilder builder = getNativeSearchQueryBuilder(searchMap);
+        NativeSearchQueryBuilder builder = buildBasicQuery(searchMap);
         //搜索并封装结果
         Map searchResult = getSearchResult(builder);
         //搜索分类和品牌
@@ -72,6 +73,11 @@ public class SkuServiceImpl implements SkuService {
         return searchResult;
     }
 
+    /**
+     * 搜索规格展示到界面上的搜索栏
+     * @param builder
+     * @return
+     */
     public Map<String, Set<String>> searchSpec(NativeSearchQueryBuilder builder) {
         builder.addAggregation(AggregationBuilders.terms("skuSpec").field("spec.keyword"));
         AggregatedPage<SkuInfo> skuInfos = elasticsearchTemplate.queryForPage(builder.build(), SkuInfo.class);
@@ -111,7 +117,7 @@ public class SkuServiceImpl implements SkuService {
         AggregatedPage<SkuInfo> skuInfos = elasticsearchTemplate.queryForPage(builder.build(), SkuInfo.class);
         //获取所有的分组查询数据
         Aggregations aggregations = skuInfos.getAggregations();
-        if (searchMap == null || searchMap.get("category") != null) {
+        if (searchMap == null || searchMap.get("category") == null) {
             //从所有数据中获取别名为skuCategory的数据
             StringTerms stringTerms = aggregations.get("skuCategory");
             //封装分类List集合，将结果存入到List中
@@ -121,7 +127,7 @@ public class SkuServiceImpl implements SkuService {
             }
             resultMap.put("categoryList",list);
         }
-        if (searchMap == null || searchMap.get("brand") != null) {
+        if (searchMap == null || searchMap.get("brand") == null) {
             //封装品牌List集合，将结果存入到List中
             StringTerms skuBrandTerms = aggregations.get("skuBrand");
             ArrayList<String> brandList = new ArrayList<>();
@@ -132,6 +138,11 @@ public class SkuServiceImpl implements SkuService {
         }
     }
 
+    /**
+     * 封装结果
+     * @param builder
+     * @return
+     */
     private Map getSearchResult(NativeSearchQueryBuilder builder) {
         NativeSearchQuery build = builder.build();
         Page<SkuInfo> skuInfos = elasticsearchTemplate.queryForPage(build, SkuInfo.class);
@@ -141,12 +152,31 @@ public class SkuServiceImpl implements SkuService {
         return resultMap;
     }
 
-    private NativeSearchQueryBuilder getNativeSearchQueryBuilder(Map<String, String> searchMap) {
+    /**
+     * 构建查询条件
+     * @param searchMap
+     * @return
+     */
+    private NativeSearchQueryBuilder buildBasicQuery(Map<String, String> searchMap) {
         NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
         if (searchMap != null) {
+            //关键字查询
             if (!StringUtils.isEmpty(searchMap.get("keywords"))) {
                 builder.withQuery(QueryBuilders.matchQuery("name",searchMap.get("keywords")));
             }
+
+            //构建bool查询 用于分类和品牌
+            BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+            if (!StringUtils.isEmpty(searchMap.get("category"))) {
+                //分类搜索 match query搜索的时候，首先会解析查询字符串，进行分词，然后查询
+                boolQueryBuilder.must(QueryBuilders.matchQuery("categoryName",searchMap.get("category")));
+            }
+            if (!StringUtils.isEmpty(searchMap.get("brand"))) {
+                //品牌搜索  term query,输入的查询内容是什么，就会按照什么去查询，并不会解析查询内容，对它分词
+                boolQueryBuilder.must(QueryBuilders.termQuery("brandName",searchMap.get("brand")));
+            }
+            //添加过滤条件
+            builder.withFilter(boolQueryBuilder);
         }
         return builder;
     }
