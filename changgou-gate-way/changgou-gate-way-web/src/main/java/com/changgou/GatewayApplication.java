9c098a7a4317a86a46788be98dc25ecdacc05ac9
zengweishi <805212031@qq.com>
2020-06-05 16:54:04 +0800

    微服务网关请求相关配置

--- changgou-gate-way/changgou-gate-way-web/src/main/java/com/changgou/GatewayApplication.java
+++ changgou-gate-way/changgou-gate-way-web/src/main/java/com/changgou/GatewayApplication.java
@@ -1,8 +1,13 @@
 package com.changgou;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 /**
  * @Auther: weishi.zeng
@@ -11,8 +16,22 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@Slf4j
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class);
+    }
+
+    @Bean(name = "ipKeyResolver")
+    public KeyResolver userKeyResolver() {
+        return new KeyResolver(){
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                //获取远程客户端IP
+                String hostString = exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
+                log.error("hostString：{}",hostString);
+                return Mono.just(hostString);
+            }
+        };
     }
 }
